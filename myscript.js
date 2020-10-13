5f73ffedae9b4e5386b5b3fafc14e58f46299efd
StefanoSerafini <stefanoserafini7@gmail.com>
2020-10-13 15:28:31 +0200

    esercizio con variabili js

---
+++ myscript.js
@@ -0,0 +1,12 @@
+
+//richiedere nome cognome e colore preferito
+//stampa a video dei tre valori +19
+
+
+var nome = prompt ("Inserisci il tuo nome");
+
+var cognome = prompt ("Inserisci il tuo cognome");
+
+var colorepreferito = prompt ("Inserisci il tuo colore preferito");
+
+document.body.innerHTML = nome + cognome + colorepreferito + "19"; 
